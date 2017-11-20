e250a12676668dc855ce32d227907b78ce2b34bc
Jep Alaba <jep.alaba@gmail.com>
2017-11-20 14:15:10 -0800

    Removed paren. from single string param funcs

--- gamemode/minigame/minigame.sv.lua
+++ gamemode/minigame/minigame.sv.lua
@@ -54,7 +54,7 @@ end
 
 util.AddNetworkString "CreateLobby"
 function MinigameService.NetworkCreateLobby(lobby)
-	net.Start("CreateLobby")
+	net.Start "CreateLobby"
 	net.WriteUInt(lobby.id, 8)
 	net.WriteUInt(lobby.prototype.id, 8)
 	net.WriteEntity(lobby.host)
@@ -63,14 +63,14 @@ end
 
 util.AddNetworkString "RemoveLobby"
 function MinigameService.NetworkRemoveLobby(lobby)
-	net.Start("RemoveLobby")
+	net.Start "RemoveLobby"
 	net.WriteUInt(lobby.id, 8)
 	net.Broadcast()
 end
 
 util.AddNetworkString "LobbyAddPlayer"
 function MinigameService.NetworkLobbyAddPlayer(lobby, ply)
-	net.Start("LobbyAddPlayer")
+	net.Start "LobbyAddPlayer"
 	net.WriteUInt(lobby.id, 8)
 	net.WriteEntity(ply)
 	net.Broadcast()
@@ -78,7 +78,7 @@ end
 
 util.AddNetworkString "LobbyRemovePlayer"
 function MinigameService.NetworkLobbyRemovePlayer(lobby, ply)
-	net.Start("LobbyRemovePlayer")
+	net.Start "LobbyRemovePlayer"
 	net.WriteUInt(lobby.id, 8)
 	net.WriteEntity(ply)
 	net.Broadcast()
