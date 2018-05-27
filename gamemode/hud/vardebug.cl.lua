2991b429a24a997bca72caff9745f736ceb7998d
Jep Alaba <jep.alaba@gmail.com>
2018-05-26 20:01:27 -0700

    Added lobby debugging

--- gamemode/hud/vardebug.cl.lua
+++ gamemode/hud/vardebug.cl.lua
@@ -26,11 +26,12 @@ end
 function VarDebug:Paint(w, h)
 	local ply = LocalPlayer()
 	local label = string.upper(self.label or self.accessor or self.variable or "?")
-	local val = string.upper(tostring(self.func and self.func() or self.accessor and ply[self.accessor](ply) or ply:GetTable()[self.variable] or "?"))
+	local val = self.func and self.func() or self.accessor and ply[self.accessor](ply) or ply:GetTable()[self.variable] or "?"
+	local val_str = string.upper(istable(val) and table.ToString(val) or tostring(val))
 
 	surface.SetFont("VarDebugFont")
 	local label_w, label_h = surface.GetTextSize(label)
-	local val_w, val_h = surface.GetTextSize(val)
+	local val_w, val_h = surface.GetTextSize(val_str)
 
 	surface.SetTextColor(COLOR_WHITE)
 	surface.SetTextPos(0, h/2 - label_h/2)
@@ -41,7 +42,7 @@ function VarDebug:Paint(w, h)
 	surface.SetDrawColor(COLOR_BLUE)
 	surface.DrawRect(val_x - val_padding, val_y + 1, val_w + (val_padding * 2), val_h)
 	surface.SetTextPos(val_x, val_y)
-	surface.DrawText(val)
+	surface.DrawText(val_str)
 end
 
 vgui.Register("VarDebug", VarDebug, "EditablePanel")
@@ -50,15 +51,25 @@ vgui.Register("VarDebug", VarDebug, "EditablePanel")
 -- # Init
 
 function VarDebugService.Init()
+	local ply = LocalPlayer()
+
 	VarDebugService.container = vgui.Create("VarDebugContainer")
 	CamUIService.AddPanel(VarDebugService.container)
+
+	VarDebugService.lobby = vgui.Create("VarDebug")
+	VarDebugService.lobby.label = "lobby.id"
+	VarDebugService.lobby.func = function () return ply.lobby and ply.lobby.id end
+
+	VarDebugService.player_class = vgui.Create("VarDebug")
+	VarDebugService.player_class.label = "player_class.name"
+	VarDebugService.player_class.func = function () return ply.player_class and ply.player_class.name end
 
 	VarDebugService.health = vgui.Create("VarDebug")
 	VarDebugService.health.accessor = "Health"
 
 	VarDebugService.speed = vgui.Create("VarDebug")
 	VarDebugService.speed.label = "Speed"
-	VarDebugService.speed.func = function () return math.Round(LocalPlayer():GetVelocity():Length()/10) end
+	VarDebugService.speed.func = function () return math.Round(ply:GetVelocity():Length()/10) end
 end
 hook.Add("InitPostEntity", "VarDebugService.Init", VarDebugService.Init)
 
