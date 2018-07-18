d1817ea71b5865c164aeb7455e23a1640b19ca95
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2018-07-18 10:47:59 -0400

    Zachary Chandler added button networking

--- gamemode/util/spectate.cl.lua
+++ gamemode/util/spectate.cl.lua
@@ -1,6 +1,7 @@
 SpectateService = SpectateService or {}
 SpectateService.UNSPECTATE_KEYS = bit.bor(IN_JUMP, IN_MOVELEFT, IN_MOVERIGHT, IN_FORWARD, IN_BACK)
 
+
 -- # Spectate
 
 function SpectateService.UnspectateCheck(ply, key)
@@ -8,7 +9,15 @@ function SpectateService.UnspectateCheck(ply, key)
 		ply:GetObserverMode() != 0 and
 		bit.band(SpectateService.UNSPECTATE_KEYS, key) != 0
 	then
-		ply:ConCommand("emm_unspectate")
+		-- ply:ConCommand("emm_unspectate")
 	end
 end
 hook.Add("KeyPress", "SpectateService.UnspectateCheck", SpectateService.UnspectateCheck)
+
+
+-- # Button Networking
+
+function SpectateService.SpectateKeysUpdate()
+	local buttons = net.ReadUInt(24)
+end
+net.Receive("Spectate Keys Update", SpectateService.SpectateKeysUpdate)
