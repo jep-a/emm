d58ef7d797491296cca93e404aca47795163f37d
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2018-07-18 10:58:14 -0400

    Zachary Chandler refactored button networking

--- gamemode/util/spectate.cl.lua
+++ gamemode/util/spectate.cl.lua
@@ -1,5 +1,12 @@
 SpectateService = SpectateService or {}
 SpectateService.UNSPECTATE_KEYS = bit.bor(IN_JUMP, IN_MOVELEFT, IN_MOVERIGHT, IN_FORWARD, IN_BACK)
+SpectateService.buttons = 0
+
+
+-- # Util
+function SpectateService.TargetKeyDown(key)
+	return bit.band(SpectateService.buttons, key)
+end
 
 
 -- # Spectate
@@ -9,7 +16,8 @@ function SpectateService.UnspectateCheck(ply, key)
 		ply:GetObserverMode() != 0 and
 		bit.band(SpectateService.UNSPECTATE_KEYS, key) != 0
 	then
-		-- ply:ConCommand("emm_unspectate")
+		ply:ConCommand("emm_unspectate")
+		SpectateService.buttons = 0
 	end
 end
 hook.Add("KeyPress", "SpectateService.UnspectateCheck", SpectateService.UnspectateCheck)
@@ -18,6 +26,6 @@ hook.Add("KeyPress", "SpectateService.UnspectateCheck", SpectateService.Unspecta
 -- # Button Networking
 
 function SpectateService.SpectateKeysUpdate()
-	local buttons = net.ReadUInt(24)
+	SpectateService.buttons = net.ReadUInt(24)
 end
 net.Receive("Spectate Keys Update", SpectateService.SpectateKeysUpdate)
