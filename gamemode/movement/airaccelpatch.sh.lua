c7589ddbd8f8caba45d5226a378b64f02c222681
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2018-07-19 14:31:51 -0400

    Zachary Chandler fixed code quality

--- gamemode/movement/airaccelpatch.sh.lua
+++ gamemode/movement/airaccelpatch.sh.lua
@@ -4,26 +4,26 @@ local AirAccelerationPatch = {}
 -- # Air Acceleration Patch
 
 function AirAccelerationPatch.SetMoveType(ply, move)
-	local zvel = move:GetVelocity().z
+	local z_vel = move:GetVelocity().z
 
 	if
-		zvel > 0
-		and zvel < 140
-		and ply:GetMoveType() == MOVETYPE_WALK
+		z_vel > 0 and
+		z_vel < 140 and
+		ply:GetMoveType() == MOVETYPE_WALK
 	then
 		ply:SetMoveType(MOVETYPE_LADDER)
 	end
 end
-hook.Add("SetupMove", "AirAccelerationPatch.SetMoveType", AirAccelerationPatch.SetMoveType)
+hook.Add("SetupMove", "AirAccelerationPatch.SetMoveType",AirAccelerationPatch.SetMoveType)
 
 function AirAccelerationPatch.RemoveLadderSound(sound)
-	if (sound.Entity != NULL) then
-		local zvel = sound.Entity:GetVelocity().z
+	if sound.Entity then
+		local z_vel = sound.Entity:GetVelocity().z
 
 		if
-			zvel > 0
-			and zvel < 140
-			and string.StartWith(sound.SoundName, "player/footsteps/ladder")
+			z_vel > 0 and
+			z_vel < 140 and
+			string.StartWith(sound.SoundName, "player/footsteps/ladder")
 		then
 			  return false
 		end
