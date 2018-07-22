2e365886045576e87e9c9cf773b670d3555c1290
Jep Alaba <jep.alaba@gmail.com>
2018-07-21 18:54:16 -0700

    Removed EnvironmentService and put it in anon function

--- gamemode/core/env.sv.lua
+++ gamemode/core/env.sv.lua
@@ -1,13 +1,7 @@
-EnvironmentService = EnvironmentService or {}
-
-
--- # Enviornment
-
-function EnvironmentService.SetupEnvironment()
+hook.Add("Initialize", "EMM.SetupEnv", function ()
 	RunConsoleCommand("sv_gravity", 300)
 	RunConsoleCommand("sv_sticktoground", 0)
 	RunConsoleCommand("sv_maxvelocity", 10000)
 	RunConsoleCommand("sv_accelerate", 10)
 	RunConsoleCommand("sv_airaccelerate", 10)
-end
-hook.Add("Initialize", "EnvironmentService.SetupEnvironment", EnvironmentService.SetupEnvironment)
+end)
