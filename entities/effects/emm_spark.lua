4f3ef7f4b4ddd78b35d4d2a832b61fad939c389a
Jep Alaba <jep.alaba@gmail.com>
2018-07-23 22:44:53 -0700

    Cleaned up effects

--- entities/effects/emm_spark.lua
+++ entities/effects/emm_spark.lua
@@ -1,17 +1,12 @@
-local Vector = Vector
-local Angle = Angle
-local random = math.random
-
 function EFFECT:Init(data)
 	local ent = data:GetEntity()
 	local origin = data:GetOrigin()
 	local ang = data:GetAngles()
 	local norm = data:GetNormal()
 	local emitter = ParticleEmitter(origin, false)
-	local color = ent.color or COLOR_WHITE
 
 	for i = 0, 20 do
-		local vel = Vector(random(0, 256), random(-128, 128), random(0, 128))
+		local vel = Vector(math.random(0, 256), math.random(-128, 128), math.random(0, 128))
 		vel:Rotate(norm:Angle())
 
 		local particle = emitter:Add("effects/spark", origin)
@@ -21,17 +16,17 @@ function EFFECT:Init(data)
 		particle:SetDieTime(1)
 		particle:SetGravity(Vector(0, 0, -800))
 		particle:SetBounce(0.25)
-		particle:SetColor(color.r, color.g, color.b)
+		particle:SetColor(ent.color.r, ent.color.g, ent.color.b)
 		particle:SetStartAlpha(255)
 		particle:SetEndAlpha(0)
-		particle:SetStartSize(random(3, 5))
-		particle:SetStartLength(random(3, 10))
+		particle:SetStartSize(math.random(3, 5))
+		particle:SetStartLength(math.random(3, 10))
 		particle:SetEndSize(0)
 		particle:SetEndLength(0)
 	end
 
 	for i = 0, 20 do
-		local vel = Vector(random(0, 256), random(-128, 256), random(-128, 256))
+		local vel = Vector(math.random(0, 256), math.random(-128, 256), math.random(-128, 256))
 		vel:Rotate(norm:Angle())
 
 		local particle = emitter:Add("effects/spark", origin)
@@ -41,11 +36,11 @@ function EFFECT:Init(data)
 		particle:SetDieTime(0.5)
 		particle:SetGravity(Vector(0, 0, -800))
 		particle:SetBounce(0.25)
-		particle:SetColor(color.r, color.g, color.b)
+		particle:SetColor(ent.color.r, ent.color.g, ent.color.b)
 		particle:SetStartAlpha(255)
 		particle:SetEndAlpha(0)
-		particle:SetStartSize(random(2, 2))
-		particle:SetStartLength(random(1, 3))
+		particle:SetStartSize(math.random(2, 2))
+		particle:SetStartLength(math.random(1, 3))
 		particle:SetEndSize(0)
 		particle:SetEndLength(0)
 	end
