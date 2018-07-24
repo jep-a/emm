0d08b87390e9ba1119a979001c5267c25dd04b99
Jep Alaba <jep.alaba@gmail.com>
2018-07-23 22:44:53 -0700

    Cleaned up effects

--- entities/effects/emm_ripple.lua
+++ entities/effects/emm_ripple.lua
@@ -1,57 +1,50 @@
-local Vector = Vector
-local Angle = Angle
-local IgnoreZ = cam.IgnoreZ
-local Start3D2D = cam.Start3D2D
-local End3D2D = cam.End3D2D
-local SetAlphaMultiplier = surface.SetAlphaMultiplier
-local SetMaterial = surface.SetMaterial
-local SetDrawColor = surface.SetDrawColor
-local DrawTexturedRect = surface.DrawTexturedRect
-
-local LIFE_SPAN = 2
+local LIFE_SPAN = 1
 
 function EFFECT:Init(data)
 	self.entity = data:GetEntity()
+	self.start_time = CurTime()
 	self.die_time = CurTime() + LIFE_SPAN
 	self.origin = data:GetOrigin()
 	self.normal = data:GetNormal()
-	self.color = self.entity.color or COLOR_WHITE
-	self.alpha = 255
+	self.color = self.entity.color
 	self.size = 0
+	self.alpha = 255
 end
 
-function EFFECT:Think()
-	local prog = (self.die_time - CurTime())
-	if prog > 0 then
-		local frac = math.EaseInOut(prog/LIFE_SPAN, 1, 0)
-		self.alpha = Lerp(frac, 0, 255)
-		self.size = Lerp(frac, 150, 0)
+local Ease = CubicBezier(0.2, 1, 0.2, 1)
 
-		return true
-	else
-		return false
+function EFFECT:Think()
+	local time = math.TimeFraction(self.start_time, self.die_time, CurTime())
+	local alive = 1 >= time
+
+	if alive then
+		local eased_time = Ease(time)
+		self.size = Lerp(eased_time, 0, 150)
+		self.alpha = Lerp(eased_time, 255, 0)
 	end
+
+	return alive
 end
 
 local CIRCLE_MATERIAL = Material("emm/indicator/circle.png", "noclamp smooth")
+
 function EFFECT:Render()
-	local ent = self.entity
-	local norm = self.normal
-	local norm_ang = norm:Angle()
-	local size = self.size
-	local vec = self.origin + (norm * Vector(0.5, 0.5, 0.5))
-	IgnoreZ(IsValid(ent) and ent.indicator)
-		SetAlphaMultiplier(self.alpha/255)
-			SetMaterial(CIRCLE_MATERIAL)
-			SetDrawColor(self.color)
+	local pos = self.origin + (self.normal/2)
+	local norm_ang = self.normal:Angle()
 
-			Start3D2D(vec, norm_ang + Angle(90, 0, 0), 0.25)
-					DrawTexturedRect(-size/2, -size/2, size, size)
-			End3D2D()
+	cam.IgnoreZ(IsValid(self.entity) and self.entity.indicator)
+	surface.SetAlphaMultiplier(self.alpha/255)
+	surface.SetMaterial(CIRCLE_MATERIAL)
+	surface.SetDrawColor(self.color)
 
-			Start3D2D(vec, norm_ang + Angle(-90, 0, 0), 0.25)
-					DrawTexturedRect(-size/2, -size/2, size, size)
-			End3D2D()
-		SetAlphaMultiplier(1)
-	IgnoreZ(false)
+	cam.Start3D2D(pos, norm_ang + Angle(90, 0, 0), 0.25)
+	surface.DrawTexturedRect(-self.size/2, -self.size/2, self.size, self.size)
+	cam.End3D2D()
+
+	cam.Start3D2D(pos, norm_ang + Angle(-90, 0, 0), 0.25)
+	surface.DrawTexturedRect(-self.size/2, -self.size/2, self.size, self.size)
+	cam.End3D2D()
+
+	surface.SetAlphaMultiplier(1)
+	cam.IgnoreZ(false)
 end
