91690a54fc6208f87e90123cafd23b744210ff0b
beebson <beeb@emm.com>
2018-07-27 13:29:55 -0400

    Added clickable property to tools

--- gamemode/build/geometry.cl.lua
+++ gamemode/build/geometry.cl.lua
@@ -16,6 +16,7 @@ function GeometryPoint:Init()
 	self.super.Init(self)
 	self.pos = Vector()
 	self.should_render = true
+	self.clickable = true
 end
 
 function GeometryPoint:Render()
@@ -37,6 +38,37 @@ function GeometryPoint:GetPos()
 end
 
 function GeometryPoint:IsHovered()
+	local box_corner = Vector(1,1,1)*(POINT_RADIUS/2)
+	local hit_pos,_,_ = util.IntersectRayWithOBB(EyePos(), EyeVector()*6000, self.pos, Angle(0), -box_corner, box_corner)
+	return (hit_pos ~= nil)
+end
+
+-- ## Edge
+
+GeometryEdge = Class.New(GeometryType)
+
+function GeometryEdge:Init()
+	self.super.Init(self)
+	self.pos = Vector()
+	self.should_render = true
+	self.clickable = true
+	self.points = {}
+end
+
+function GeometryEdge:Render()
+    if self.should_render == false then return end
+end
+Class.AddHook(GeometryEdge, "PostDrawTranslucentRenderables", "Render")
+
+function GeometryEdge:SetPos(position)
+	self.pos = position
+end
+
+function GeometryEdge:GetPos()
+	return self.pos
+end
+
+function GeometryEdge:IsHovered()
 	local box_corner = Vector(1,1,1)*(POINT_RADIUS/2)
 	local hit_pos,_,_ = util.IntersectRayWithOBB(EyePos(), EyeVector()*6000, self.pos, Angle(0), -box_corner, box_corner)
 	return (hit_pos ~= nil)
--- gamemode/build/tools/create-point.cl.lua
+++ gamemode/build/tools/create-point.cl.lua
@@ -7,13 +7,15 @@ TOOL.show_name      = "Create Point"
 
 TOOL.description    = [[
     Left click to place a point.
-    Scroll up and down to change the distance of the point.
+    Scroll up and down to change the tool distance of the point.
 ]]
 
 function TOOL:OnEquip()
     for _, point in pairs(BuildObjects.Points) do
         point.should_render = true
     end
+
+    chat.AddText(self.description)
 end
 
 function TOOL:OnHolster()
@@ -23,13 +25,12 @@ function TOOL:OnHolster()
 end
 
 function TOOL:Render()
-    local point_pos = BuildService.GetToolPosition()
-    render.DrawWireframeSphere(point_pos, 5, 10, 10, Color(255,255,255,255))
+    BuildUtil.RenderToolCursor()
 end
 
 TOOL.Press[IN_ATTACK] = function()
     local new_point = GeometryPoint.New()
-    new_point:SetPos(BuildService.GetToolPosition())
+    new_point:SetPos(BuildUtil.GetToolPosition())
     new_point.should_render = true
     table.insert(BuildObjects.Points, new_point)
 end
