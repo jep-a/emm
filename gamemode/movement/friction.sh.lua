db5a081c637d8fd2de5beb10f8c820196965b6f8
Pivius <pivisens@gmail.com>
2019-03-27 19:48:23 +0100

    Cleanup

--- gamemode/movement/friction.sh.lua
+++ gamemode/movement/friction.sh.lua
@@ -4,6 +4,7 @@ FrictionService = FrictionService or {}
 -- # Properties
 
 CreateConVar("emm_friction", 8, FCVAR_REPLICATED, "Player friction")
+
 function FrictionService.GetDefaultFriction()
 	return GetConVar("emm_friction"):GetFloat()
 end
@@ -21,36 +22,42 @@ hook.Add(
 -- # Friction
 
 function FrictionService.Velocity(friction, move)
-  local vel = move:GetVelocity()
-  local	speed = vel:Length()
-  local	drop = 0
-  local newspeed, control
-  if (speed < GetConVar("sv_stopspeed"):GetFloat()) then
-    control = GetConVar("sv_stopspeed"):GetFloat()
-  else
-    control = speed
-  end
-  drop = drop + (control * friction * FrameTime())
-  newspeed = speed - drop
-  if newspeed < 0 then
-  	newspeed = 0
-  end
-  if newspeed != speed then
-  	newspeed = newspeed / speed
-    vel = vel * newspeed
-  end
-  return vel
+	local vel = move:GetVelocity()
+	local	speed = vel:Length()
+	local	drop = 0
+	local newspeed, control
+
+	if (GetConVar("sv_stopspeed"):GetFloat() > speed) then
+		control = GetConVar("sv_stopspeed"):GetFloat()
+	else
+		control = speed
+	end
+
+	drop = drop + (control * friction * FrameTime())
+	newspeed = speed - drop
+
+	if 0 > newspeed then
+		newspeed = 0
+	end
+
+	if newspeed != speed then
+		newspeed = newspeed/speed
+		vel = vel * newspeed
+	end
+
+	return vel
 end
 
 
 function FrictionService.SetupFriction(ply, move)
-  if !ply.lobby and ply.friction != FrictionService.GetDefaultFriction() then
-    ply.friction = FrictionService.GetDefaultFriction()
-  end
-  if move:GetVelocity():Length() < 0.1 or ply:GetGroundEntity() == NULL or (move:KeyPressed(IN_JUMP) and not move:KeyWasDown(IN_JUMP)) then
-    return
-  end
+	if not ply.lobby and ply.friction != FrictionService.GetDefaultFriction() then
+		ply.friction = FrictionService.GetDefaultFriction()
+	end
 
-  move:SetVelocity(FrictionService.Velocity(ply.friction, move))
+	if 0.1 > move:GetVelocity():Length() or ply:GetGroundEntity() == NULL or (move:KeyPressed(IN_JUMP) and not move:KeyWasDown(IN_JUMP)) then
+		return
+	end
+
+	move:SetVelocity(FrictionService.Velocity(ply.friction, move))
 end
 hook.Add("SetupMove", "FrictionService.SetupFriction", FrictionService.SetupFriction)
--- gamemode/movement/gravity.sh.lua
+++ gamemode/movement/gravity.sh.lua
@@ -4,13 +4,13 @@ GravityService = GravityService or {}
 -- # Properties
 
 CreateConVar("emm_gravity", 300, FCVAR_REPLICATED, "Player gravity.")
+
 function GravityService.GetDefaultGravity()
 	return GetConVar("emm_gravity"):GetFloat()
 end
 
 function GravityService.InitPlayerProperties(ply)
 	ply.gravity = 300
-  ply.axis = Vector(0, 0, -1)
 end
 hook.Add(
 	SERVER and "InitPlayerProperties" or "InitLocalPlayerProperties",
@@ -22,21 +22,24 @@ hook.Add(
 -- # Gravity
 
 function GravityService.Velocity(ply, move)
-  local gravity = Vector(1, 1, 1) * (ply.gravity * FrameTime())
-  gravity = gravity * ply.axis
-  return move:GetVelocity() + gravity
+	local gravity = Vector(0, 0, (ply.gravity * FrameTime()))
+
+	return move:GetVelocity() - gravity
 end
 
 function GravityService.SetupGravity(ply, move)
-  if !ply.lobby and ply.gravity != GravityService.GetDefaultGravity() then
-    ply.gravity = GravityService.GetDefaultGravity()
-  end
-  if GetConVar("sv_gravity"):GetFloat() != 0 then
-    local mult = GetConVar("sv_gravity"):GetFloat() / (GravityService.GetDefaultGravity() * 200)
-    ply:SetGravity(mult)
-    move:SetVelocity(move:GetVelocity() + Vector(0, 0, GravityService.GetDefaultGravity() * mult * FrameTime()))
-  end
-  if ply:WaterLevel() > 1 or ply.gravity == 0  then return end
-  move:SetVelocity(GravityService.Velocity(ply, move))
+	if not ply.lobby and ply.gravity != GravityService.GetDefaultGravity() then
+		ply.gravity = GravityService.GetDefaultGravity()
+	end
+
+	if GetConVar("sv_gravity"):GetFloat() != 0 then
+		local mult = GetConVar("sv_gravity"):GetFloat()/(GravityService.GetDefaultGravity() * 200)
+
+		ply:SetGravity(mult)
+		move:SetVelocity(move:GetVelocity() + Vector(0, 0, GravityService.GetDefaultGravity() * mult * FrameTime()))
+	end
+
+	if ply:WaterLevel() > 1 or ply.gravity == 0 then return end
+	move:SetVelocity(GravityService.Velocity(ply, move))
 end
 hook.Add("SetupMove", "GravityService.SetupGravity", GravityService.SetupGravity)
