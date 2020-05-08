4e0fde533307f711e20f352644bc7805d62ffa18
beebson <beeb@emm.com>
2020-05-08 18:45:26 -0400

    Implement ChatNetService functions
    - ChatNetService.ReqCreateVoiceChannel
    - ChatNetService.ReqCreateTextChannel
    - ChatNetService.ReqJoinChannel

--- gamemode/chat/net.sv.lua
+++ gamemode/chat/net.sv.lua
@@ -1,44 +1,71 @@
-ChatService = ChatService or {}
+ChatNetService = ChatNetService or {}
 
 -- Request Handlers
 --- Handle request to create a voice channel
 ---@param creator Player | "Creator of the channel"
 ---@param private bool | "Should the server be private?"
-function ChatService.ReqCreateVoiceChannel(creator, private)
+function ChatNetService.ReqCreateVoiceChannel(creator, private)
     -- Check that the creator doesn't have a channel open already
-    -- Create new channel
-    -- Set channel host to [creator]
-    -- Set channel private to [private]
-    -- Remove player from their current channel
-    -- Add player to the new channel
-    -- Broadcast new channel information
+    local has_channel = false
+    local current_channel = {}
+    -- TODO: Find better way to do this
+    for channel_id, channel in pairs(ChatService.channels) do
+        if channel:HasPlayer(creator) then
+            current_channel = channel
+        end
+        if channel.host == creator and channel:InstanceOf(VoiceChannel) then
+            has_channel = true
+        end
+    end
+
+    if not has_channel then
+        ChatService.RemovePlayer(current_channel, creator)
+        ChatService.CreateVoiceChannel(creator, private)
+    end
 end
-NetService.Receive("ReqCreateVoiceChannel", ChatService.ReqCreateVoiceChannel)
+NetService.Receive("ReqCreateVoiceChannel", ChatNetService.ReqCreateVoiceChannel)
 
 --- Handle request to create a text channel
 ---@param creator Player | "Creator of the channel"
 ---@param private bool | "Is the channel private?"
-function ChatService.ReqCreateTextChannel(creator, private)
-    -- Create new channel
-    -- Set channel host to [creator]
-    -- Set channel private to [private]
-    -- Remove player from their current channel
-    -- Add player to the new channel
-    -- Broadcast new channel information
+function ChatNetService.ReqCreateTextChannel(creator, private)
+    -- Check that the creator doesn't have a channel open already
+    local has_channel = false
+    local current_channel = {}
+    -- TODO: Find better way to do this
+    for channel_id, channel in pairs(ChatService.channels) do
+        if channel:HasPlayer(creator) then
+            current_channel = channel
+        end
+        if channel.host == creator and channel:InstanceOf(TextChannel) then
+            has_channel = true
+        end
+    end
+
+    if not has_channel then
+        ChatService.CreateTextChannel(creator, private)
+    end
 end
-NetService.Receive("ReqCreateTextChannel", ChatService.ReqCreateTextChannel)
+NetService.Receive("ReqCreateTextChannel", ChatNetService.ReqCreateTextChannel)
 
 --- Handle request to join a channel
 ---@param ply Player | "Player requesting to join channel"
 ---@param channel ChatChannel | "Channel to put the player into"
 function ChatNetService.ReqJoinChannel(ply, channel)
-    -- Check if the target channel is private
-    -- Remove the player from their current channel
-    -- Broadcast player left signal
-    -- Add the player to the new channel
-    -- Broadcast player joined signal
+    -- Check if the target channel is private or the player is banned
+    if channel.private or channel:CheckBan(ply) then return end
+    local current_channel = {}
+    for channel_id, current_channel in pairs(ChatService.channels) do
+        if current_channel:HasPlayer(creator) then
+            ChatService.RemovePlayer(current_channel, ply)
+            break
+        end
+    end
+    
+    channel:AddPlayer(ply)
+    NetService.Broadcast("PlayerJoinChannel", channel, ply)
 end
-NetService.Receive("ReqJoinChannel", ChatService.ReqJoinChannel)
+NetService.Receive("ReqJoinChannel", ChatNetService.ReqJoinChannel)
 
 --- Handle request to invite a player to a channel
 ---@param ply Player | "Player requesting to invite"
@@ -47,13 +74,14 @@ NetService.Receive("ReqJoinChannel", ChatService.ReqJoinChannel)
 function ChatNetService.ReqChannelInvite(ply, channel, recipient)
     -- Check if the ply has op flag
     -- Send SendChannelInvite -> recipient + everyone in the channel
+    
 end
-NetService.Receive("ReqChannelInvite", ChatService.ReqChannelInvite)
+NetService.Receive("ReqChannelInvite", ChatNetService.ReqChannelInvite)
 
 --- Handle request to leave a channel
 ---@param ply Player | "Player requesting to leave"
 ---@param channel ChatChannel | "Channel the player is leaving"
-function ChatService.ReqLeaveChannel(ply, channel)
+function ChatNetService.ReqLeaveChannel(ply, channel)
     -- Check if the channel is one of the public channels
     -- Remove player from current channel
     -- Broadcast PlayerLeaveChannel
@@ -61,7 +89,7 @@ function ChatService.ReqLeaveChannel(ply, channel)
         -- Put player in public channel
         -- Broadcast PlayerJoinChannel
 end
-NetService.Receive("ReqLeaveChannel", ChatService.ReqLeaveChannel)
+NetService.Receive("ReqLeaveChannel", ChatNetService.ReqLeaveChannel)
 
 --- Handle request to accept invite to chat channel
 ---@param recipient Player | "Player that accepted the invite"
@@ -77,16 +105,6 @@ NetService.Receive("ReqAcceptChatInvite", ChatNetService.ReqAcceptChatInvite)
 --- Handle request to sync lobbies to a player
 ---@param ply Player | "Player to send the lobbies to"
 function ChatNetService.ReqSyncLobbies(ply) 
-    -- Send lobby data
+    NetService.Send("SyncLobbyData",ply, ChatService.ChannelsToPacket())
 end
-NetService.Receive("ReqSyncLobbies", ChatService.ReqSyncLobbies)
-
-
--- -- ?? All the player is doing is asking to be in the public ??
--- -- ?? channel so should we keep this request                ??
--- --- Handle request to leave a channel
--- ---@param ply Player | "Player requesting to leave"
--- ---@param channel ChatChannel | "Channel the player is leaving"
--- function ChatNetService.ReqLeaveChannel(ply, channel)
--- end
--- NetService.Receive("ReqLeaveChannel", ChatNetService.ReqLeaveChannel)
+NetService.Receive("ReqSyncLobbies", ChatNetService.ReqSyncLobbies)
