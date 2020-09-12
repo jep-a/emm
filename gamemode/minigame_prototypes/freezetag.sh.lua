555bd8f1ef4785efc04e6fecccd95f66a05b9896
uh014083 <70987605+uh014083@users.noreply.github.com>
2020-09-12 01:42:33 +0100

    Added "frozen for" timer, changed default class

--- gamemode/minigame_prototypes/freezetag.sh.lua
+++ gamemode/minigame_prototypes/freezetag.sh.lua
@@ -1,6 +1,6 @@
 MINIGAME.name = "Freezetag"
 MINIGAME.color = COLOR_RASPBERRY
-MINIGAME.default_player_class = "Runner"
+MINIGAME.default_player_class = "Tagger"
 MINIGAME.required_players = 2
 
 MINIGAME.states.Playing.time = 60 * 5
@@ -46,13 +46,60 @@ MINIGAME:AddAdjustableSetting {
 	label = "Runner can be frozen from damage"
 }
 
+hook.Add("CreateMinigameHookSchemas", "Freezetag", function ()
+    MinigameNetService.CreateHookSchema("SetFrozen", {"entity"})
+    MinigameNetService.CreateHookSchema("SetTagger", {"entity"})
+end)
+
+if CLIENT then
+    MINIGAME:AddHookNotification("SetFrozen", function (self, involves_local_ply, tagger)
+		if involves_local_ply then
+			LocalPlayer().freeze_timer = NotificationService.PushCountdown(CurTime() + FREEZE_TIME, "frozen for", "FrozenCountdown")
+        end
+	end)
+    MINIGAME:AddHookNotification("SetTagger", function (self, involves_local_ply, tagger)
+		if involves_local_ply then
+			NotificationService.PushText("you have become a tagger")
+		else
+			NotificationService.PushSideText(tagger:Nick() .. " has become a tagger")
+		end
+    end)
+    timer.Create("freeze_timer", 1, 0, function()
+        local ply = LocalPlayer()
+        if ply.freeze_timer then
+            if not (ply.player_class and ply.player_class.name == "Frozen") then
+                ply.freeze_timer:Finish()
+            end
+        end
+    end)
+end
+
 if SERVER then
     function MINIGAME:Tag(taggable, tagger)
         timer.Remove("freeze_" .. tagger:SteamID()) -- Remove any previous created timers for turning frozen into tagger
-        timer.Create("timer_for_player_class_on_delay_" .. tagger:SteamID(), FREEZE_TIME, 1, function()
+        timer.Create("freeze_" .. tagger:SteamID(), FREEZE_TIME, 1, function()
             if (tagger.player_class and tagger.player_class.name == "Frozen") then
                 tagger:SetPlayerClass(taggable.lobby:GetPlayerClass("Tagger"))
+                MinigameService.CallNetHookWithoutMethod(self, "SetTagger", tagger)
             end
         end)
+        if (taggable.player_class and taggable.player_class.player_class_on_tag == "Frozen") then
+            MinigameService.CallNetHookWithoutMethod(self, "SetFrozen", tagger)
+        end
+    end
+end
+
+local lobby = nil
+
+for k, v in pairs(player:GetAll()) do
+    if (k == 1) then
+        lobby = v.lobby
+	else
+		if (v.lobby) then
+			v.lobby:RemovePlayer(v)
+		end
+		if (lobby) then
+			lobby:AddPlayer(v)
+		end
     end
 end
