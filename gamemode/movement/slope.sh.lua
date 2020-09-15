8aa2ea97eaa89f21697719150b08bc95fa90e1b7
Pivius <pivisens@gmail.com>
2020-09-15 23:39:26 +0200

    Optimization

--- gamemode/movement/slope.sh.lua
+++ gamemode/movement/slope.sh.lua
@@ -6,7 +6,7 @@ SlopeService = SlopeService or {}
 function SlopeService.InitPlayerProperties(ply)
 	ply.bounce_height = 12
 	ply.slope_onground = false
-	ply.last_vel = Vector()
+	ply.old_velocity = Vector()
 end
 hook.Add(
 	SERVER and "InitPlayerProperties" or "InitLocalPlayerProperties",
@@ -18,12 +18,12 @@ hook.Add(
 -- # Slope Boost
 
 function SlopeService.AddSpeed(normal, ply, move)
-	if 1 > normal.z and ply:OnGround() and not ply.slope_onground and 0 >= ply.last_vel.z then
-		local last_vel = ply.last_vel
+	if 1 > normal.z and ply:OnGround() and not ply.slope_onground and 0 >= ply.old_velocity.z then
+		local old_velocity = ply.old_velocity
 		local dot, vel
 		
-		last_vel.z = last_vel.z - (ply.gravity * FrameTime() * 0.5)
-		vel = last_vel - (normal * last_vel:Dot(normal))
+		old_velocity.z = old_velocity.z - (ply.gravity * FrameTime() * 0.5)
+		vel = old_velocity - (normal * old_velocity:Dot(normal))
 		dot = vel:Dot(normal)
 
 		if 0 > dot then
@@ -31,9 +31,9 @@ function SlopeService.AddSpeed(normal, ply, move)
 		end
 
 		vel.z = 0
-		last_vel.z = 0
+		old_velocity.z = 0
 
-		if vel:LengthSqr() > last_vel:LengthSqr() then
+		if vel:LengthSqr() > old_velocity:LengthSqr() then
 			move:SetVelocity(vel)
 		end
 	end
@@ -45,39 +45,42 @@ end
 function SlopeService.LedgeBounce(ply, move)
 	local pos = move:GetOrigin()
 	local vel = move:GetVelocity()
+	local pred_vel = vel * FrameTime() * 4
 	local bounce_height = Vector(0, 0, ply.bounce_height)
-	local pred_vel = (vel * FrameTime() * 4)
-	local ledge_trace = util.TraceHull {
+	local bottom_trace = util.TraceHull {
 		start = pos,
 		endpos = pos + pred_vel,
 		mins = ply:OBBMins(),
 		maxs = Vector(ply:OBBMaxs().x, ply:OBBMaxs().y) + bounce_height,
 		mask = MASK_PLAYERSOLID_BRUSHONLY
 	}
-	local wall_trace = util.TraceHull {
-		start = pos + bounce_height,
-		endpos = pos + bounce_height + pred_vel,
-		mins = ply:OBBMins(),
-		maxs = ply:OBBMaxs() - bounce_height,
-		mask = MASK_PLAYERSOLID_BRUSHONLY
 	
-	}
-	
-	if 
-		ledge_trace.HitWorld and
-		1 > ledge_trace.Fraction and 
-		ledge_trace.Fraction > 0 and 
-		ledge_trace.HitNormal.z == 0 and 
-		not wall_trace.HitWorld and 
-		vel.z > -50 and 
-		vel:Length2D() > 500 and 
-		ledge_trace.HitNormal:Dot(AiraccelService.WishDir(ply, move):GetNormalized()) < 0.5
-	then
-		return ledge_trace.HitNormal
+	if bottom_trace.HitWorld and bottom_trace.HitNormal.z == 0 and vel.z > -50 and vel:Length2D() > 500 then
+		local can_bounce = bottom_trace.HitNormal:Dot(AiraccelService.WishDir(ply, move):GetNormalized())
+		local top_trace = util.TraceHull {
+			start = pos + bounce_height,
+			endpos = pos + bounce_height + pred_vel,
+			mins = ply:OBBMins(),
+			maxs = ply:OBBMaxs() - bounce_height,
+			mask = MASK_PLAYERSOLID_BRUSHONLY
+		}
+
+		if 
+			1 > bottom_trace.Fraction and 
+			bottom_trace.Fraction > 0 and 
+			not top_trace.HitWorld and  
+			can_bounce < 0.5 and
+			can_bounce ~= 0
+		then
+			return bottom_trace.HitNormal
+		end
 	end
 	
 	return false
 end
+
+
+-- # Hooks
 
 function SlopeService.SetupSlope(ply, move)
 	local ledge_normal = SlopeService.LedgeBounce(ply, move) 
@@ -97,6 +100,6 @@ function SlopeService.SetupSlope(ply, move)
 		move:SetVelocity(SlideService.Clip(move:GetVelocity(), normal))
 	end
 
-	ply.last_vel = move:GetVelocity()
+	ply.old_velocity = move:GetVelocity()
 end
 hook.Add("Move", "SlopeService.SetupSlope", SlopeService.SetupSlope)
