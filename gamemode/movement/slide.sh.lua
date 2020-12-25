62a6bf45f19a77872de7006f1e912abeb77ed0f0
Pivius <pivisens@gmail.com>
2020-12-25 19:22:10 +0100

    Modified 'SetupSlide' to apply the proper velocity for every surface it find and cleanup

--- gamemode/movement/slide.sh.lua
+++ gamemode/movement/slide.sh.lua
@@ -18,7 +18,7 @@ hook.Add(
 )
 
 
-MAX_CLIP_PLANES = 5
+SLIDE_MAX_TRACES = 4
 
 -- # Util
 
@@ -66,7 +66,7 @@ function SlideService.Trace(ply, vel, pos)
 			end
 		else
 			if 0 > vel.z and not ply:OnGround() then
-				trace = SlideService.GetGroundTrace(pos, slide_pos + (ramp_normal * Vector(2, 2, -2)) + Vector(0, 0, math.min(pred_vel.z, 0)), ply)
+				trace = SlideService.GetGroundTrace(pos, slide_pos + pred_vel, ply)
 			else
 				trace = SlideService.GetGroundTrace(pos, slide_pos - ((ply:OnGround() and hover_height) or Vector()), ply)
 			end
@@ -88,20 +88,23 @@ function SlideService.Trace(ply, vel, pos)
 			if corr_trace.HitNormal ~= trace.HitNormal and corr_trace.HitNormal.z > 0 and 1 > corr_trace.HitNormal.z then
 				trace.HitNormal = (corr_trace.HitNormal + trace.HitNormal)/2
 			end
+
 			return trace
 		end
 	end
 	return false
 end
 
-function SlideService.ShouldSlide(ply, normal, vel, vel_z)
+function SlideService.ShouldSlide(ply, normal, vel)
+	local slide_vel = SlideService.Clip(vel, normal)
+
 	if 
 		(0 > Vector(normal.x, normal.y):Dot(Vector(vel.x, vel.y):GetNormalized()) and
 		1 > normal.z and
 		normal.z > ply.slide_minimum and
 		vel:Dot(vel) > 900 and
 		((not ply.sliding and 
-		vel_z and vel_z > 150 or vel.z > 150) or 
+		slide_vel.z and slide_vel.z > 150 or vel.z > 150) or 
 		ply.sliding))
 	then
 		return true
@@ -123,7 +126,9 @@ end
 -- # Sliding
 
 function SlideService.SlideStrafe(move, normal)
-	if normal:Dot(AiraccelService.WishDir(ply, move):GetNormalized()) > 0 and move:GetVelocity():Dot(normal) > 0 then
+	local wish_dir = AiraccelService.WishDir(ply, move:GetMoveAngles():Forward(), move:GetForwardSpeed(), move:GetSideSpeed())
+
+	if normal:Dot(wish_dir:GetNormalized()) > 0 and move:GetVelocity():Dot(normal) > 0 then
 		return true
 	end
 
@@ -134,15 +139,15 @@ function SlideService.Slide(ply, move, trace, slide_vel)
 	if not ((ply.sliding or ply.surfing) and 0 > (slide_vel.z - move:GetVelocity().z)) then
 		ply.sliding = (SlideService.ShouldSlide(ply, trace.HitNormal, slide_vel) and trace) or false
 		ply.surfing = (SlideService.ShouldSurf(ply, trace.HitNormal, slide_vel) and trace) or false
+	end
 
-		if (move:GetVelocity().z >= 0 and ply.sliding) or ply.surfing then
+	if (move:GetVelocity().z >= 0 and ply.sliding) or ply.surfing then
 			local pos = move:GetOrigin()
 			
 			pos.z = trace.HitPos.z + ply.slide_hover_height
 			ply:SetGroundEntity(NULL)
 			move:SetVelocity(slide_vel)
 			move:SetOrigin(pos)
-		end
 	end
 end
 
@@ -152,7 +157,7 @@ function SlideService.HandleSlideDamage(ply)
 	local trace = SlideService.GetGroundTrace(pos, pos + (vel * FrameTime()) - Vector(0, 0, ply.slide_hover_height), ply)
 	local slide_vel = SlideService.Clip(vel, trace.HitNormal)
 
-	if (SlideService.ShouldSlide(ply, trace.HitNormal, vel, slide_vel.z) or SlideService.ShouldSurf(ply, trace.HitNormal, vel)) then
+	if (SlideService.ShouldSlide(ply, trace.HitNormal, vel) or SlideService.ShouldSurf(ply, trace.HitNormal, vel)) then
 		pos.z = trace.HitPos.z + ply.slide_hover_height
 		ply.slide_onground = {slide_vel, pos}
 		ply:SetGroundEntity(NULL)
@@ -161,11 +166,8 @@ end
 hook.Add("OnPlayerHitGround", "SlideService.HandleSlideDamage", SlideService.HandleSlideDamage)
 
 function SlideService.SetupSlide(ply, move, cmd)
-	local vel = move:GetVelocity()
-	local pos = move:GetOrigin()
-	local trace, corr_trace = SlideService.Trace(ply, vel, pos, move)
-	local slide_vel = Vector()
-	local should_slide = false
+	local traces = {}
+	local trace_count = 0
 
 	if ply.slide_onground then
 		move:SetVelocity(ply.slide_onground[1])
@@ -173,28 +175,54 @@ function SlideService.SetupSlide(ply, move, cmd)
 		ply.slide_onground = false
 	end
 
-	if trace then
-		slide_vel = SlideService.Clip(vel, trace.HitNormal)
-		should_slide = SlideService.ShouldSlide(ply, trace.HitNormal, vel, slide_vel.z) or SlideService.ShouldSurf(ply, trace.HitNormal, vel)
+	for i = 1, SLIDE_MAX_TRACES do
+		local vel = move:GetVelocity()
+		local pos = move:GetOrigin()
+		local trace = SlideService.Trace(ply, vel, pos)
 
-		if 1 > trace.HitNormal.z and not trace.StartSolid and should_slide then
-			if SlideService.SlideStrafe(move, trace.HitNormal) or ply:OnGround() then
-				ply:SetGroundEntity(NULL)
-				move:SetVelocity(vel)
-				move:SetOrigin(pos + Vector(0, 0, ply.slide_hover_height))
-			else
-				SlideService.Slide(ply, move, trace, slide_vel)
+		if trace then
+			trace.should_slide = SlideService.ShouldSlide(ply, trace.HitNormal, vel) or SlideService.ShouldSurf(ply, trace.HitNormal, vel)
+
+			if trace_count >= 1 then
+				local is_same_plane = false
+
+				for j = 1, trace_count do
+					if i > j then
+						if traces[j].HitNormal == trace.HitNormal and trace.HitNormal.z > 0 and 1 > trace.HitNormal.z then
+							is_same_plane = true
+							break
+						end
+					end
+				end
+
+				if is_same_plane then
+					continue
+				end
+			end
+
+			if 1 > trace.HitNormal.z and not trace.StartSolid and trace.should_slide then
+				trace_count = trace_count + 1
+				traces[trace_count] = trace
+
+				if SlideService.SlideStrafe(move, trace.HitNormal) or ply:OnGround() then
+					ply:SetGroundEntity(NULL)
+					move:SetVelocity(vel)
+					move:SetOrigin(move:GetOrigin() + Vector(0, 0, ply.slide_hover_height))
+				else
+					SlideService.Slide(ply, move, trace, SlideService.Clip(vel, trace.HitNormal))
+				end
 			end
 		end
-
-		if not trace.StartSolid then
-			SlopeService.AddSpeed(trace.HitNormal, ply, move)
+		
+		if not trace or (trace and not trace.should_slide) then
+			ply.sliding = false
+			ply.surfing = false
 		end
-	end
-
-	if not trace or (trace and not should_slide) then
-		ply.sliding = false
-		ply.surfing = false
 	end
 end
 hook.Add("SetupMove", "SlideService.SetupSlide", SlideService.SetupSlide)
+
+hook.Add( "PlayerNoClip", "FeelFreeToTurnItOff", function( ply, desiredState )
+
+	return true -- always allow
+end )
