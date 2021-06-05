21d624e6c15971e37d603f71ea39ed367f25994d
Pivius <pivisens@gmail.com>
2021-06-05 14:13:18 +0200

    Jump file renamed and cleanup

--- gamemode/core/gamemode.sh.lua
+++ gamemode/core/gamemode.sh.lua
@@ -28,7 +28,7 @@ EMM.Include {
 	"movement/walljump",
 	"movement/wallslide",
 	"movement/airaccel",
-	"movement/autojump",
+	"movement/jump",
 	"movement/airaccel-patch",
 	"movement/slide",
 	"movement/friction",
